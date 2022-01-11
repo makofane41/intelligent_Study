4059a5f389f0571a3714d109cb6ac0bac0dc3980
given <makofane411@gmail.com>
2022-01-11 10:39:30 +0200

    activities

---
+++ app/src/main/java/com/example/geek/q_a_Activity.java
@@ -0,0 +1,14 @@
+package com.example.geek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class q_a_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qa);
+    }
+}
